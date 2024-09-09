969b0a23d4f7169168322ab9c61218eb5b9ee417
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2024-09-09 15:53:28 +0200

    updated dep and styles

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,12 @@
-import Core from "@sk-web-gui/core";
-import ContainerQueries from "@tailwindcss/container-queries";
+import { preset as Core } from "@sk-web-gui/core";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit",
   content: [
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+
     "./node_modules/@sk-web-gui/*/dist/**/*.js",
   ],
   darkMode: "class", // or 'media' or 'class'
@@ -28,11 +26,7 @@ export default {
       },
     },
   },
-  plugins: [
-    Core({
-      cssBase: false,
-      colors: [],
-    }),
-    ContainerQueries,
+  presets: [
+    Core({ plugin: { cssBase: false, components: ["AICornerModule"] } }),
   ],
 };
