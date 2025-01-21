514676bf0f95a7cc32655e9a984fd79bc9efb970
Jerker Osatie <jerker.osatie@devize.se>
2025-01-21 13:32:47 +0100

    fix: rerender bug

--- sitevision/src/components/assistant/assistant.component.tsx
+++ sitevision/src/components/assistant/assistant.component.tsx
@@ -6,16 +6,26 @@ export interface AssistantProps {
   assistant: AssistantInfo;
   settings: AssistantSettings;
   shadowdom?: boolean;
+  apiBaseUrl: string;
+  stream: boolean;
 }
 
 export const Assistant: React.FunctionComponent<AssistantProps> = ({
   assistant,
   settings,
   shadowdom = true,
+  stream = true,
+  apiBaseUrl,
 }) => {
-  const [oldInfo, setInfo, oldSettings, setSettings] = useAssistantStore(
-    (state) => [state.info, state.setInfo, state.settings, state.setSettings]
-  );
+  const [oldInfo, setInfo, oldSettings, setSettings, setStream, setApiBaseUrl] =
+    useAssistantStore((state) => [
+      state.info,
+      state.setInfo,
+      state.settings,
+      state.setSettings,
+      state.setStream,
+      state.setApiBaseUrl,
+    ]);
 
   React.useEffect(() => {
     require("../../../assets/assistant-wisser");
@@ -28,7 +38,8 @@ export const Assistant: React.FunctionComponent<AssistantProps> = ({
       ...assistant,
     };
     setInfo(info);
-  }, [assistant, setInfo, oldInfo]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [assistant, setInfo]);
 
   React.useEffect(() => {
     if (settings) {
@@ -38,7 +49,16 @@ export const Assistant: React.FunctionComponent<AssistantProps> = ({
       };
       setSettings(newSettings);
     }
-  }, [settings, setSettings, oldSettings]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [settings, setSettings]);
+
+  React.useEffect(() => {
+    setStream(stream);
+  }, [stream, setStream]);
+
+  React.useEffect(() => {
+    setApiBaseUrl(apiBaseUrl);
+  }, [apiBaseUrl, setApiBaseUrl]);
 
   return (
     <div>
